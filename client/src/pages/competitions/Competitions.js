eb27ac2b5a25cc099ffe48c919d5ce3ca301206f
Januar Soepangat <januar.soepangat@gmail.com>
2022-12-15 08:08:51 -0600

    Add competitions page

--- client/src/pages/competitions/Competitions.js
+++ client/src/pages/competitions/Competitions.js
@@ -1,25 +1,63 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useState, useEffect } from "react";
 import UserContext from "../../auth/UserContext";
+import ShowdownApi from "../../api/api";
+import CompetitionCard from "./CompetitionCard";
+
 import "./Competitions.css";
 
-/** Competitions of site.
+/** Show page with list of competitions.
  * 
- * Shows welcome message
+ * On mount, loads competitions from API.
+ * Re-loads filtered competitions on submit from search form.
  * 
- * Routed at /
+ * Routed at /competitions
  * 
- * Routes => Competitions
+ * Routes => { CompetitionCard, SearchForm }
  */
 
 function Competitions() {
   const { currentUser } = useContext(UserContext);
-  console.debug("Competitions", "currentUser:", currentUser);
+  const [competitions, setCompetitions] = useState([]);
+
+  console.debug(
+    "Competitions", 
+    "currentUser:", currentUser
+  );
+
+  useEffect(function getCompetitionsOnMount() {
+    console.debug("Competitions useEffect getCompetitionsOnMount");
+    search();
+  }, []);
+
+  async function search(competition_handle) {
+    let competitions = await ShowdownApi.getCompetitions(competition_handle);
+    setCompetitions(competitions);
+  }
 
   return (
-    <div className="Competitions">
+    <div className="Competitions col-md-8 offset-md-2">
       <div className="container text-center">
         <h1 className="mb-4 font-weight-bold">Competitions</h1>
+
+        { competitions.length 
+          ? (
+            <div>
+              <div className="Competitions-list row">
+                {competitions.map(c => (
+                  <CompetitionCard 
+                    key={c.competitionHandle}
+                    competitionHandle={c.competitionHandle}
+                    competitionName={c.competitionName}
+                    description={c.description}
+                    gender={c.gender}
+                    logoUrl={c.logoUrl}
+                  />
+                ))}
+              </div>
+            </div>
+          )
+          : "" }
+
       </div>
 
     </div>
